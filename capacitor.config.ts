db0f5e950ae3c351242eb2f877bfa5e2c0c58199
Gustavo Cortez <cmgustavo83@gmail.com>
2025-08-06 22:45:40 -0300

    Add icon and splash images

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'simple-tetris',
+  appId: 'com.simpletetrisng',
+  appName: 'Tetris Simple',
   webDir: 'www'
 };
 
